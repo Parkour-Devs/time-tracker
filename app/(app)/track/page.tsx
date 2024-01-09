329c160b28c8bb78d08c2be05db0bf7368abbc42
Hernan Valencia <frjr17@criptext.com>
2024-01-08 19:03:11 -0500

    fix: including project and client in activity returned data

--- app/(app)/track/page.tsx
+++ app/(app)/track/page.tsx
@@ -1,78 +1,78 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
   SelectLabel,
-  SelectTrigger
-} from '@/components/ui/select'
-import { getUserSession } from '@/lib/auth'
-import { prisma } from '@/lib/prisma'
-import { cn } from '@/lib/utils'
-import { Activity, Client, Project } from '@prisma/client'
-import { Building2, FolderOpenDot, Play, Square } from 'lucide-react'
-import { revalidatePath } from 'next/cache'
-import { ActivityItemRow } from './activity-item-row'
-import { ActivityDuration } from './duration'
+  SelectTrigger,
+} from "@/components/ui/select";
+import { getUserSession } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { cn } from "@/lib/utils";
+import { Activity, Client, Project } from "@prisma/client";
+import { Building2, FolderOpenDot, Play, Square } from "lucide-react";
+import { revalidatePath } from "next/cache";
+import { ActivityItemRow } from "./activity-item-row";
+import { ActivityDuration } from "./duration";
 
 type TimeProps = {
-  startAt: string
-}
+  startAt: string;
+};
 
 type NewActivityProps = {
-  activity?: Activity | null
-  clients: Client[]
-  projects: Project[]
-}
+  activity?: Activity | null;
+  clients: Client[];
+  projects: Project[];
+};
 
 const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
   async function upsertActivity(data: FormData) {
-    'use server'
-    const user = await getUserSession()
-    const client = data.get('client') as string
-    const project = data.get('project') as string
+    "use server";
+    const user = await getUserSession();
+    const client = data.get("client") as string;
+    const project = data.get("project") as string;
 
     await prisma.activity.upsert({
       where: {
-        id: data.get('id') as string
+        id: data.get("id") as string,
       },
       create: {
         user: { connect: { id: user.id } },
         tenant: { connect: { id: user.tenant.id } },
-        name: data.get('name') as string,
+        name: data.get("name") as string,
         startAt: new Date(),
         client: !!client ? { connect: { id: client } } : undefined,
-        project: !!project ? { connect: { id: project } } : undefined
+        project: !!project ? { connect: { id: project } } : undefined,
       },
       update: {
-        name: data.get('name') as string,
+        name: data.get("name") as string,
         client: !!client ? { connect: { id: client } } : undefined,
-        project: !!project ? { connect: { id: project } } : undefined
-      }
-    })
-    revalidatePath('/track')
+        project: !!project ? { connect: { id: project } } : undefined,
+      },
+    });
+    revalidatePath("/track");
   }
 
   async function stopActivity(data: FormData) {
-    'use server'
+    "use server";
 
-    const client = data.get('client') as string
-    const project = data.get('project') as string
+    const client = data.get("client") as string;
+    const project = data.get("project") as string;
 
     await prisma.activity.update({
       where: {
-        id: data.get('id') as string
+        id: data.get("id") as string,
       },
       data: {
         endAt: new Date(),
-        name: data.get('name') as string,
+        name: data.get("name") as string,
         client: !!client ? { connect: { id: client } } : undefined,
-        project: !!project ? { connect: { id: project } } : undefined
-      }
-    })
-    revalidatePath('/track')
+        project: !!project ? { connect: { id: project } } : undefined,
+      },
+    });
+    revalidatePath("/track");
   }
 
   return (
@@ -80,8 +80,8 @@ const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
       <h2 className="text-lg font-medium mb-2">What are you working on?</h2>
       <form action={activity ? stopActivity : upsertActivity} className="">
         <div className="flex items-center space-x-4">
-          <Input type="text" name="name" defaultValue={activity?.name || ''} />
-          <input type="hidden" name="id" defaultValue={activity?.id || ''} />
+          <Input type="text" name="name" defaultValue={activity?.name || ""} />
+          <input type="hidden" name="id" defaultValue={activity?.id || ""} />
           <Select name="client">
             <SelectTrigger className="w-[50px]">
               <Building2 size={32} />
@@ -118,19 +118,19 @@ const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
           <Button
             type="submit"
             variant="outline"
-            className={cn('rounded-full px-2 h-[40px] w-[40px]')}
+            className={cn("rounded-full px-2 h-[40px] w-[40px]")}
           >
             {activity ? <Square size={20} /> : <Play size={20} />}
           </Button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 type DailyActivitiesProps = {
-  activities: Activity[]
-}
+  activities: Activity[];
+};
 
 const DailyActivities = ({ activities }: DailyActivitiesProps) => {
   return (
@@ -142,37 +142,37 @@ const DailyActivities = ({ activities }: DailyActivitiesProps) => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default async function TrackPage() {
-  const user = await getUserSession()
+  const user = await getUserSession();
   const currentActivity = await prisma.activity.findFirst({
     where: {
       tenantId: user.tenant.id,
       userId: user.id,
-      endAt: null
-    }
-  })
+      endAt: null,
+    },
+  });
 
   const clients = await prisma.client.findMany({
     where: {
-      tenantId: user.tenant.id
-    }
-  })
+      tenantId: user.tenant.id,
+    },
+  });
 
   const projects = await prisma.project.findMany({
     where: {
-      tenantId: user.tenant.id
-    }
-  })
+      tenantId: user.tenant.id,
+    },
+  });
 
-  const now = new Date()
+  const now = new Date();
   const startOfToday = new Date(
     now.getFullYear(),
     now.getMonth(),
     now.getDate()
-  )
+  );
   const endOfToday = new Date(
     now.getFullYear(),
     now.getMonth(),
@@ -180,23 +180,27 @@ export default async function TrackPage() {
     23,
     59,
     59
-  )
+  );
 
   const dailyActivities = await prisma.activity.findMany({
     where: {
       tenantId: user.tenant.id,
       userId: user.id,
       startAt: {
-        gte: startOfToday
+        gte: startOfToday,
       },
       endAt: {
-        lte: endOfToday
-      }
+        lte: endOfToday,
+      },
     },
     orderBy: {
-      startAt: 'asc'
-    }
-  })
+      startAt: "asc",
+    },
+    include: {
+      project: true,
+      client: true,
+    },
+  });
 
   return (
     <div className="mx-auto container py-4 space-y-12">
@@ -207,5 +211,5 @@ export default async function TrackPage() {
       />
       <DailyActivities activities={dailyActivities} />
     </div>
-  )
+  );
 }
